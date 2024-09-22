78e22d1b1170d05333dcd4113b10156a52254c9f
solo-nko <solowingrazgriz@outlook.com>
2024-09-22 23:37:54 +0900

    Add ability to delete entries from database.

---
+++ src/components/ConfirmDialog.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+defineEmits(['confirm', 'cancel'])
+
+</script>
+
+<template>
+<VCard>
+	<VLabel>Are you sure?</VLabel>
+	<VCardActions>
+		<VBtn @click="$emit('confirm')">Yes</VBtn>
+		<VBtn @click="$emit('cancel')">Cancel</VBtn>
+	</VCardActions>
+</VCard>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/DatabaseView.vue
+++ src/views/DatabaseView.vue
@@ -4,6 +4,7 @@ import { useObservable } from '@vueuse/rxjs';
 import { appDatabase } from '../database/db.ts';
 import { ref } from 'vue';
 import EntryDialogGames from '../components/EntryDialogGames.vue';
+import ConfirmDialog from '../components/ConfirmDialog.vue';
 
 // ref may be required
 const games = useObservable(liveQuery(() => appDatabase.games.toArray()));
@@ -17,10 +18,21 @@ const gameHeaders = [
 ];
 
 const showEditDialog = ref(false)
+const showDeleteDialog = ref(false)
 const entryDetails = ref()
 function editEntry(entryInfo) {
 	showEditDialog.value = true;
 	entryDetails.value = entryInfo;
+}
+
+function deleteEntryConfirmation(entryInfo) {
+	showDeleteDialog.value = true;
+	entryDetails.value = entryInfo;
+}
+
+async function deleteEntry() {
+	appDatabase.games.delete(entryDetails.value.id)
+	showDeleteDialog.value = false;
 }
 
 </script>
@@ -29,10 +41,14 @@ function editEntry(entryInfo) {
 	<VDataTable :headers="gameHeaders" :items="games" items-per-page="10">
 		<template v-slot:item.actions="{item}">
 			<VLabel @click="editEntry(item)">Edit</VLabel>
+			<VLabel @click="deleteEntryConfirmation(item)">Delete</VLabel>
 		</template>
 	</VDataTable>
 	<VDialog v-model="showEditDialog">
 		<EntryDialogGames @close-entry="showEditDialog = false" :game-entry="entryDetails" :edit-entry="true" ></EntryDialogGames>
+	</VDialog>
+	<VDialog v-model="showDeleteDialog">
+		<ConfirmDialog @confirm="deleteEntry" @cancel="showDeleteDialog = false"></ConfirmDialog>
 	</VDialog>
 </template>
 
