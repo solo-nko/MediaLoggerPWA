3f3e2f221fe69e27cd80bd91d75b93eecfae54a4
solo-nko <solowingrazgriz@outlook.com>
2024-09-22 13:42:02 +0900

    Add static methods for serializing objects

--- types/Log.ts
+++ types/Log.ts
@@ -1,10 +1,29 @@
 import { DateTime } from "luxon"
 import DatabaseSchema from '../src/database/DatabaseSchema';
 import { Entity } from 'dexie';
+import { Delta } from 'quill/core';
 
 export default class Log extends Entity<DatabaseSchema> {
 	id!: number | null
 	title: string
-	dateCreated: string | null
-	dateModified: string | null
+	dateCreated: DateTime | null
+	dateModified: DateTime | null
+	impression: Delta | string | null
+
+	static dateToString(dateObject: DateTime) {
+		return dateObject.toSQL()
+	}
+
+	static dateFromString(dateString: string) {
+		return DateTime.fromSQL()
+	}
+
+	static impressionToString(impressionObject: Delta) {
+		return JSON.stringify(impressionObject)
+	}
+
+	static impressionFromString(impressionString: string) {
+		return JSON.parse(impressionString)
+	}
+
 }
