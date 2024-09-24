5e122af95f62d772b4a2414981ec33426d31c376
solo-nko <solowingrazgriz@outlook.com>
2024-09-24 22:59:29 +0900

    Implement database import (no UI yet)

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -4,6 +4,7 @@ import 'dexie-export-import';
 import { saveAs } from 'file-saver';
 
 import { appDatabase } from '../database/db.ts';
+import { peakImportFile } from 'dexie-export-import';
 
 function progressCallback({ totalRows, completedRows }) {
 	console.log(`Progress: ${completedRows} / ${totalRows} rows completed`);
@@ -13,6 +14,20 @@ async function exportDatabase() {
 	const blob = await appDatabase.export({ prettyJson: true, progressCallback });
 	const fileName = 'database.json';
 	saveAs(blob, fileName);
+}
+
+async function importDatabase(file) {
+	const importMetadata = await peakImportFile(file);
+		if (importMetadata.formatName != 'dexie') throw new Error('Invalid format');
+		console.log('Database name:', importMetadata.data.databaseName);
+		console.log('Database version:', importMetadata.data.databaseVersion);
+		console.log('Database version:', importMetadata.data.databaseVersion);
+		console.log(
+			'Tables:',
+			importMetadata.data.tables.map((t) => `${t.name} (${t.rowCount} rows)`).join('\n\t')
+		);
+	await appDatabase.delete();
+	await appDatabase.import(file);
 }
 </script>
 
